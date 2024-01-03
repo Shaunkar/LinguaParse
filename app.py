87a360a463206ce3db833fbadc50535d8937e450
SHANKAR SUWAN <shankarsuwan2000@gmail.com>
2024-01-03 22:32:14 +0530

    Your commit message here

--- app.py
+++ app.py
@@ -1,35 +1,31 @@
 import streamlit as st
-from config import get_api_key
-from models import VisionModel
-from utils import process_image
+from PIL import Image
+from config.config import GOOGLE_API_KEY
+from models.vision_model import VisionModel
+from utils.image_utils import input_image_details
 
-# Configure page title
 st.set_page_config(page_title="Multilanguage Invoice Extractor")
 
-# Create an instance of the VisionModel
-api_key = get_api_key()
-model = VisionModel(api_key)
-
-# App layout and elements
 st.header("Multilanguage Invoice Extractor")
+input_prompt = """
+You are an expert in understanding invoices. We will upload an image as an invoice, 
+and you will have to answer any questions based on the uploaded invoice image.
+"""
 
 input_text = st.text_input("Input Prompt: ", key="input")
 uploaded_file = st.file_uploader("Choose an image of the invoice...", type=["jpg", "jpeg", "png"])
 
+image = ""
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
 
-if st.button("Tell me about the Invoice"):
-    try:
-        image_parts = process_image(uploaded_file)
-        prompt = """
-        You are an expert in understanding invoices. We will upload an image as invoice and you will have to answer any questions based on the uploaded invoice image.
-        """
-        response = model.generate_response(input_text, image_parts, prompt)
+submit = st.button("Tell me about the Invoice")
 
-        st.subheader("The Response is")
-        st.write(response)
-
-    except ValueError as e:
-        st.error(e)
+if submit:
+    image_data = input_image_details(uploaded_file)
+    vision_model = VisionModel()
+    response = vision_model.generate_content([input_prompt, image_data[0], input_text])
+    
+    st.subheader("The Response is")
+    st.write(response.text)
